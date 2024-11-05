eb9b352011fc4ee64b6071ad7b1d21fa7c096588
Nielton Didi <nieltondidi@gmail.com>
2024-11-05 08:22:01 +0800

    feat: created nav item component

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let title: string = '';
+	export let url: string = '';
+	export let active: boolean = false;
+
+	export let onClick = () => {};
+</script>
+
+<li class={`flex h-full flex-col items-center justify-center w-full md:w-fit`}>
+	<a
+		href={url}
+		on:click={onClick}
+		class="group flex h-full flex-col items-center justify-center px-2 w-full md:w-fit"
+	>
+		<span
+			class={`text-md font-normal lowercase ${active ? `text-dark-neutral-90` : `text-dark-neutral-100`} transition-all duration-300 group-hover:text-dark-neutral-90`}
+		>
+			{title}
+		</span>
+	</a>
+</li>
