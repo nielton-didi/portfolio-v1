4fcc344f1e482292eef17c97bb328d084373d3d9
Nielton Didi <nieltondidi@gmail.com>
2024-11-06 08:22:24 +0800

    feat: created image card for photo gallery

---
+++ src/lib/components/ImageCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div
+	class={`aspect-[4/3] h-28 rounded-xl border border-light-neutral-20 bg-light-neutral-10 p-1.5 shadow shadow-light-neutral-40 hover:shadow-lg hover:scale-105`}
+>
+	<div
+		class={`h-full w-full rounded-lg bg-cover bg-center`}
+		style="background-image: url('https://images.wallpaperscraft.com/image/single/man_backpack_anonymous_167107_1920x1080.jpg')"
+	></div>
+</div>
