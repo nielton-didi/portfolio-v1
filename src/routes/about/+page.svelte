de0c45733b7b05b15c5437f11ed8b80da00cb6ea
Nielton Didi <nieltondidi@gmail.com>
2024-11-06 08:24:10 +0800

    feat: WIP about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
+	import ImageCard from "$lib/components/ImageCard.svelte";
+
 </script>
 
-<section class={`flex h-screen w-full flex-col items-center justify-start bg-light-neutral-20`}>
-	<div
-		class={`flex h-3/4 w-full flex-col items-center justify-end bg-light-neutral-10 shadow shadow-light-neutral-40`}
+<div class={`flex w-full flex-col items-center justify-start bg-light-neutral-20`}>
+	<section
+		class={`mt-14 flex h-fit min-h-fit w-full flex-col items-center justify-center bg-light-neutral-10 shadow-sm`}
 	>
-		<div class={`h-full w-full max-w-[1040px] border-b border-light-neutral-30`}></div>
-		<div class={`w-full max-w-[1040px] h-40 min-h-40`}></div>
-	</div>
-</section>
+		<div class={`flex w-full max-w-[1040px] flex-row items-center justify-start border-b border-light-neutral-30 mt-24`}>
+			<div class={`flex w-1/2 flex-col items-start justify-center`}>
+				<h1 class={`mb-2 text-[56px] font-bold leading-tight text-light-neutral-100`}>about.</h1>
+				<p class={`mb-6 text-xl font-light leading-tight text-light-neutral-60`}>
+					I'm a software developer based in Sarawak, Malaysia
+				</p>
+				<p class={`text-justify text-lg font-light leading-normal text-light-neutral-70 mb-6`}>
+					Since 2020, I've been passionate about creating all kinds of applications—web, mobile, and
+					even desktop. When I'm not coding, you'll find me cooking, drawing or on the badminton
+					court.
+				</p>
+			</div>
+		</div>
+		<!-- photo gallery -->
+		<div class={`w-full max-w-[1040px] flex flex-row justify-between items-center flex-wrap py-8`}>
+            <ImageCard></ImageCard>
+            <ImageCard></ImageCard>
+            <ImageCard></ImageCard>
+            <ImageCard></ImageCard>
+            <ImageCard></ImageCard>
+            <ImageCard></ImageCard>
+        </div>
+	</section>
+	<section class={`h-screen w-full`}></section>
+</div>
