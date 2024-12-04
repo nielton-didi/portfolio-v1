ff3a3ecbf4404369cd3abf440e040a91ef5ff7c1
Nielton Didi <nieltondidi@gmail.com>
2024-12-05 01:58:54 +0800

    feat: created experience card component

---
+++ src/lib/components/ExperienceCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { ExperienceDTO } from '$lib/dto/system.dto';
+	import Pills from './Pills.svelte';
+
+	export let experience: ExperienceDTO;
+</script>
+
+<div
+	class={`flex w-full flex-row items-start justify-start gap-6 rounded-md bg-light-neutral-0 py-6`}
+>
+	<!-- leading -->
+	<div class={`flex w-full flex-col items-start justify-start`}>
+		<p class={`text-md font-normal leading-tight text-light-neutral-70`}>
+			
+		</p>
+		<p class={`text-[40px] font-bold tracking-wide leading-tight text-light-neutral-110`}>
+			{experience.position}
+		</p>
+		<p class={`mt-4 text-lg font-light leading-tight text-light-neutral-70`}>
+			{experience.companyName} — {experience.startYear} to {experience.endYear}
+		</p>
+	</div>
+	<!-- trailing -->
+	<div class={`flex w-full flex-col items-start justify-start gap-10`}>
+		<p class={`tracking-wide text-lg font-light leading-relaxed text-light-neutral-80`}>
+			{experience.jobDescription}
+		</p>
+		<div class={`flex w-full flex-row flex-wrap items-center justify-start gap-2`}>
+			{#each experience.skills as skill}
+				<Pills title={skill}></Pills>
+			{/each}
+		</div>
+	</div>
+</div>
